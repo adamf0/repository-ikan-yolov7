b67422b31d1ce0e573d237c2cc6b2ab10d6e3bca
!Github@251423! <studiocode52@gmail.com>
2024-06-05 16:54:39 +0700

    modified:   resources/views/template.blade.php

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -27,7 +27,7 @@
                         <a href="{{ route('about') }}" class="nav__link {{\App\Helper\Utility::stateMenu(['about'],request())? 'nav__link--button':''}}">About</a>
                     </li>
                     <li class="nav__item">
-                        <a href="{{ route('login') }}" class="nav__link">Login</a>
+                        <a href="{{ route('login.index') }}" class="nav__link">Login</a>
                     </li>
                 </ul>
             </nav>
