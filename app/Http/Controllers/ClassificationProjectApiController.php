53827545cc726822a0876e834c64eabcf6627494
!Github@251423! <studiocode52@gmail.com>
2024-06-24 16:06:37 +0700

    debug

--- app/Http/Controllers/ClassificationProjectApiController.php
+++ app/Http/Controllers/ClassificationProjectApiController.php
@@ -126,7 +126,7 @@ class ClassificationProjectApiController extends Controller
             return json_encode([
                 "status"=>"fail",
                 "message"=>"ada masalah pada proses aplikasi",
-                "log"=>$e->getTraceAsString(),
+                "log"=>$e->getTrace(),
                 "data"=>$request->all(),
             ]);
         }
