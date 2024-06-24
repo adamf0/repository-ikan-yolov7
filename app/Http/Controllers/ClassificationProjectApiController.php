b3749bcf895d450fc7d87a240aed43c2d167570a
!Github@251423! <studiocode52@gmail.com>
2024-06-24 15:19:58 +0700

    debug

--- app/Http/Controllers/ClassificationProjectApiController.php
+++ app/Http/Controllers/ClassificationProjectApiController.php
@@ -126,7 +126,7 @@ class ClassificationProjectApiController extends Controller
             return json_encode([
                 "status"=>"fail",
                 "message"=>"ada masalah pada proses aplikasi",
-                "log"=>$request->file('image'),
+                "log"=>$request->file('image')?->getPathName(),
                 "data"=>[],
             ]);
         }
