de782a3f008d9bd9f3429fd5ed057c0982d0b048
!Github@251423! <studiocode52@gmail.com>
2024-07-03 08:50:27 +0700

    bug

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -26,7 +26,7 @@
             </div>
             @else
             <div class="col-3 col-md">
-                <div class="rounded-2 border p-1 text-center h-100" style="background: black; color: #cc3333">
+                <div class="rounded-2 border p-1 text-center h-100" style="background: #666666">
                     <div style="height: 40px;" class="font-small border-bottom">NOT EVALUATED</div>
                     <div class="fw-bold">NE</div>
                 </div>
@@ -48,7 +48,7 @@
             </div>
             @else
             <div class="col-3 col-md">
-                <div class="rounded-2 border p-1 text-center h-100" style="background: black;">
+                <div class="rounded-2 border p-1 text-center h-100" style="background: #999999; color: black">
                     <div style="height: 40px;" class="font-small border-bottom">DATA DEFICIENT</div>
                     <div class="fw-bold">DD</div>
                 </div>
@@ -114,7 +114,7 @@
             </div>
             @else
             <div class="col-3 col-md">
-                <div class="rounded-2 border p-1 text-center h-100" style="background: #cc9900; coloor: white">
+                <div class="rounded-2 border p-1 text-center h-100" style="background: #cc9900; color: white">
                     <div style="height: 40px;" class="font-small border-bottom">VULNERABLE</div>
                     <div class="fw-bold">VU</div>
                 </div>
@@ -180,7 +180,7 @@
             </div>
             @else
             <div class="col-3 col-md">
-                <div class="rounded-2 border p-1 text-center h-100" style="background: #006666; color: white">
+                <div class="rounded-2 border p-1 text-center h-100" style="background: black;">
                     <div style="height: 40px;" class="font-small border-bottom">EXTINCT IN THE WILD</div>
                     <div class="fw-bold">EW</div>
                 </div>
@@ -202,7 +202,7 @@
             </div>
             @else
             <div class="col-3 col-md">
-                <div class="rounded-2 border p-1 text-center h-100" style="background: #006666; color: white">
+                <div class="rounded-2 border p-1 text-center h-100" style="background: black; color: #cc3333">
                     <div style="height: 40px;" class="font-small border-bottom">EXTINCT</div>
                     <div class="fw-bold">EX</div>
                 </div>
