5b1f5f5283b0e2bfa68479a067fa6a24d769729c
!Github@251423! <studiocode52@gmail.com>
2024-07-05 11:08:51 +0700

    bug

--- resources/views/project/detail.blade.php
+++ resources/views/project/detail.blade.php
@@ -216,6 +216,8 @@
         grid-column: 1/-1;
     }
 </style>
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 @stop
 
 @section('content')
@@ -596,6 +598,7 @@
 @stop
 
 @section('script')
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script type="text/javascript" src="{{ \App\Helper\Utility::loadAsset('my.js') }}"></script>
 <script>
     $(document).ready(function() {
