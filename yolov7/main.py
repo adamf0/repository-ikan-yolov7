252f9db6c42af13f94d646e440a06e4b8a9a6e61
!Github@251423! <studiocode52@gmail.com>
2024-08-07 14:03:02 +0700

    update cors

--- yolov7/main.py
+++ yolov7/main.py
@@ -28,7 +28,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 origins = [
-    "http://localhost:8000",  # Allow requests from this origin
+    "http://www.fishiden.com",  # Allow requests from this origin
 ]
 
 app.add_middleware(
