a88dcc574c4cd1dc2ece12726683150681d54c7a
!Github@251423! <studiocode52@gmail.com>
2024-08-08 09:35:50 +0700

    debug

--- yolov7/main.py
+++ yolov7/main.py
@@ -17,6 +17,7 @@ from bs4 import BeautifulSoup
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 @asynccontextmanager
